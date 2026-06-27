7c2b4944c03c5f3cc7a0d27e9a2f9f41d38e9fd1
Amal C <150137897+amalchandralal@users.noreply.github.com>
2026-06-27 22:23:46 +0530

    Time: 2 ms (99.94%), Space: 52.9 MB (18.14%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,15 +1,38 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        List<Integer> list = new ArrayList<>();
-        int limit = nums.length/3;
-        for(int num : nums){
-            int freq = (map.getOrDefault(num,0)+1);
-            map.put(num,freq);
-            if(freq == limit+1){
-                list.add(num);
+        int count1 = 0;
+        int count2 = 0;
+        int ans1 = 0;
+        int ans2 = 0;
+        for(int num: nums){
+            if(ans1 == num){
+                count1++;
+            }else if(ans2 == num){
+                count2++;
+            }else if(count1 == 0){
+                ans1 = num;
+                count1++;
+            }else if(count2 == 0){
+                ans2 = num;
+                count2++;
+            }else{
+                count1--;
+                count2--;
+            }    
+        }
+        count1 = 0;
+        count2 = 0;
+        for(int num: nums){
+            if(num == ans1){
+                count1++;
+            }else if(num == ans2){
+                count2++;
             }
-        }return list;
-        
+        }
+        List<Integer> ans = new ArrayList<>();
+        int limit = (nums.length/3);
+        if(count1 > limit ) ans.add(ans1);
+        if(count2 > limit) ans.add(ans2);
+        return ans;
     }
 }

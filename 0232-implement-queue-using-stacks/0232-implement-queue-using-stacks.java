2d2b7da5256ded9873584f0d5e7e7d1102f65a0b
Amal C <150137897+amalchandralal@users.noreply.github.com>
2026-07-26 17:34:32 +0530

    Time: 1 ms (27.38%), Space: 42.5 MB (93.67%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -1,38 +1,45 @@
-import java.util.Stack;
-
 class MyQueue {
-
-    private Stack<Integer> input;
-    private Stack<Integer> output;
-
+    Stack<Integer>s1;
+    Stack<Integer>s2;
     public MyQueue() {
-        input = new Stack<>();
-        output = new Stack<>();
+        s1 = new Stack<>();
+        s2 = new Stack<>();
     }
-
+    
     public void push(int x) {
-        input.push(x);
-    }
-
-    private void transfer() {
-        if (output.isEmpty()) {
-            while (!input.isEmpty()) {
-                output.push(input.pop());
-            }
+        while(!s1.isEmpty()){
+            s2.push(s1.pop());
         }
-    }
+        s1.push(x);
+        while(!s2.isEmpty()){
+            s1.push(s2.pop());
+        }
 
+        
+    }
+    
     public int pop() {
-        transfer();
-        return output.pop();
+        return s1.pop();
+        
     }
-
+    
     public int peek() {
-        transfer();
-        return output.peek();
+        return s1.peek();
+        
     }
-
+    
     public boolean empty() {
-        return input.isEmpty() && output.isEmpty();
+        return s1.isEmpty();
+
+        
     }
 }
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */

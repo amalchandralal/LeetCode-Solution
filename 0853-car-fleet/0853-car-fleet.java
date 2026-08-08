6687ff139bf634cb7c41bfabe2559c8224a4b19c
Amal C <150137897+amalchandralal@users.noreply.github.com>
2026-08-08 14:31:25 +0530

    Time: 88 ms (31.77%), Space: 96 MB (70.89%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.java
+++ 0853-car-fleet/0853-car-fleet.java
@@ -1,22 +1,23 @@
 class Solution {
     public int carFleet(int target, int[] position, int[] speed) {
-        int cars[][] = new int[speed.length][2];
+        int n = position.length;
+        int [][]cars = new int[n][2];
 
-        for(int i = 0; i < cars.length ; i++){
+        for(int i = 0 ; i < cars.length ; i++){
             cars[i][0] = position[i];
             cars[i][1] = speed[i];
         }
 
-        Arrays.sort(cars, (a,b)->b[0]-a[0]);
+        Arrays.sort(cars, (a,b)-> b[0]-a[0]);
+
         Stack<Double> stack = new Stack<>();
-        for(int i = 0; i < position.length ;i++){
+
+        for(int i = 0 ;i < n ;i++){
             double distance = (double)(target-cars[i][0])/cars[i][1];
             if(stack.isEmpty() || distance > stack.peek()){
                 stack.push(distance);
             }
-
         }
-
         return stack.size();
         
     }

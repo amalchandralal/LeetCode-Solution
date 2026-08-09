80ae94e3992b99fa09afa5abf27a678f9c517c8f
Amal C <150137897+amalchandralal@users.noreply.github.com>
2026-08-09 08:31:30 +0530

    Time: 92 ms (5.92%), Space: 106.7 MB (61.36%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,24 +1,25 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[]ans = new int[n];
         Stack<Integer> helperStack = new Stack<>();
-        int len = temperatures.length;
-        int []res = new int[len];
 
-        //iterating the stack from right to left
-        for(int idx = len-1 ; idx >= 0 ; idx--){
-            //if the stack is not empty and current element is less than the stack top then pop the stack elements until we get a greater element;
-            while(!helperStack.isEmpty() && temperatures[helperStack.peek()] <= temperatures[idx]){
+        for(int idx = n-1 ; idx >= 0 ; idx--){
+            
+            while(!helperStack.isEmpty() && temperatures[idx] >= temperatures[helperStack.peek()]){
                 helperStack.pop();
             }
-            // if the stack top is greater than the element , find the differnece in the index and push to result array
-            if(!helperStack.isEmpty() ){
-                res[idx] = helperStack.peek() - idx;
-            }
 
-            helperStack.push(idx);
+            if(!helperStack.isEmpty() && temperatures[idx] < temperatures[helperStack.peek()]){
+                ans[idx] = helperStack.peek()-idx;
+                helperStack.push(idx);
+                
+            }
+            if(helperStack.isEmpty()){
+                helperStack.push(idx);
+            }
         }
-        //returning res
-        return res;
+        return ans;
         
     }
 }

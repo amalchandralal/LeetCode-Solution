65f3d6efdbaa91cd93388b844b06c35a8bd45bcb
Amal C <150137897+amalchandralal@users.noreply.github.com>
2026-08-12 23:59:29 +0530

    Time: 27 ms (94.63%), Space: 146.8 MB (57.31%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,24 +1,25 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        ArrayDeque<Integer> deque = new ArrayDeque<>();
-        int []result = new int[nums.length-k+1];
-        for(int i = 0 ;i < k;i++){
-            while(!deque.isEmpty() && nums[i] > nums[deque.peekLast()]){
-                deque.pollLast();
+        int n = nums.length;
+        Deque<Integer> queue = new ArrayDeque<>();
+        int []result = new int[n-k+1];
+        for(int i = 0 ; i < k ; i++){
+            while(!queue.isEmpty() && nums[i] > nums[queue.peekLast()]){
+                queue.pollLast();
             }
-            deque.offerLast(i);
+            queue.offerLast(i);
         }
-        result[0] = nums[deque.peekFirst()];
-        for(int i = k ;i<nums.length;i++){
-            if(deque.peekFirst() <= i-k){
-                deque.pollFirst();
-            }
-            while(!deque.isEmpty() && nums[i] > nums[deque.peekLast()]){
-                deque.pollLast();
-            }
-            deque.offerLast(i);
-            result[i-k+1] = nums[deque.peekFirst()];
+        result[0] = nums[queue.peekFirst()];
 
+        for(int i = k ; i < n ; i++){
+            if(i-k == queue.peekFirst()){
+                queue.pollFirst();
+            }
+            while(!queue.isEmpty() && nums[i] > nums[queue.peekLast()]){
+                queue.pollLast();
+            }
+            queue.offerLast(i);
+            result[i-k+1] = nums[queue.peekFirst()];
         }
         return result;
         

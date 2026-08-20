3ca4df2bf42bad99edaf5fa1d3dfb75a32add42e
Amal C <150137897+amalchandralal@users.noreply.github.com>
2026-08-20 21:19:42 +0530

    Time: 1 ms (99.52%), Space: 43.6 MB (95.38%) - LeetHub

--- 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.java
+++ 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.java
@@ -1,21 +1,37 @@
 class Solution {
     public int maxFrequencyElements(int[] nums) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int num: nums){
-            map.put(num, map.getOrDefault(num,0)+1);
-        }       
+        // HashMap<Integer,Integer> map = new HashMap<>();
+        // for(int num: nums){
+        //     map.put(num, map.getOrDefault(num,0)+1);
+        // }       
 
+        // int maxFreq = 0;
+        // for(int freq : map.values()){
+        //     maxFreq = Math.max(maxFreq,freq);
+        // } 
+        // int count = 0;
+        // for(int freq : map.values()){
+        //     if(freq == maxFreq){
+        //         count+=maxFreq;
+        //     }
+        // }
+
+        // return count;
+        int []freq = new int[101];
+        for(int num: nums){
+            freq[num]++;
+        }
         int maxFreq = 0;
-        for(int freq : map.values()){
-            maxFreq = Math.max(maxFreq,freq);
-        } 
+        for(int num: freq){
+            maxFreq = Math.max(maxFreq,num);
+
+        }
         int count = 0;
-        for(int freq : map.values()){
-            if(freq == maxFreq){
+        for(int num: freq ){
+            if(num == maxFreq){
                 count+=maxFreq;
             }
         }
-
         return count;
     }
 }
